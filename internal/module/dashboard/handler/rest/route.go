8eeccce862b44e28820daff4209cd0d401eb3012
Muhammad Ikhsan Hilmi <ikhsanhilmimuhammad@gmail.com>
2025-05-28 13:04:15 +0700

    feat: add rbac midleware dashboard pos

--- internal/module/dashboard/handler/rest/route.go
+++ internal/module/dashboard/handler/rest/route.go
@@ -3,5 +3,5 @@ package rest
 import "github.com/gofiber/fiber/v2"
 
 func (h *dashboardHandler) DashboardRoute(superadminRouter fiber.Router) {
-	superadminRouter.Get("/dashboard", h.middleware.AuthBearer, h.getDashboard)
+	superadminRouter.Get("/dashboard", h.middleware.AuthBearer, h.middleware.RBACMiddleware("read", "dashboard_pos"), h.getDashboard)
 }
